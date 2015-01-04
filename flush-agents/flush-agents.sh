136b61434ed122de193b0b9ca58728f0ee217e2d
Soleblaze <soleblaze@exploits.sexy>
2015-01-04 11:44:14 -0600

    removed user specific.  should flush it for all users

--- flush-agents/flush-agents.sh
+++ flush-agents/flush-agents.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-user=soleblaze
 
 # remove saved passphrases in gpg-agent
-/usr/bin/pkill -u $user -SIGHUP gpg-agent
+/usr/bin/pkill -SIGHUP gpg-agent
 
 # Remove all keys from ssh-agent
-for SSH_AUTH_SOCK in `find /tmp/ssh-*/agent.* -user $user`; do
+for SSH_AUTH_SOCK in `find /tmp/ssh-*/agent.*`; do
     SSH_AUTH_SOCK=$SSH_AUTH_SOCK /usr/bin/ssh-add -D
 done
