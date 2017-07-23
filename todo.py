c5702f7bb1ea85d8fa239aa3681a4c7eb2280f2d
soleblaze <soleblaze@exploits.sexy>
2017-07-22 19:03:13 -0500

    Fix linter errors

--- todo.py
+++ todo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+""" lists and adds tasks to todoist """
 
 import os
 import sys
-import requests
 import todoist
 import yaml
 
@@ -10,9 +10,9 @@ import yaml
 def connect():
     """ Connect to todoist """
     # Get API Token for todoist
-    fh = open(os.path.expanduser("~/.todoist.yml"), "r")
-    config = yaml.load(fh)
-    fh.close()
+    config_file = open(os.path.expanduser("~/.todoist.yml"), "r")
+    config = yaml.load(config_file)
+    config_file.close()
 
     # Connect to todoist
     api = todoist.TodoistAPI()
@@ -23,18 +23,21 @@ def connect():
         msg = user.get('error')
         print("Error {}: {}".format(code, msg))
         exit(1)
-
     return api
 
-def help():
-    help = "add [project] [task] - adds task to project\n"
-    help += "list - lists all projects and their items\n"
-    help += "list [project] - lists items associated with that project\n"
-    help += "projects - lists projects"
-    print(help)
+
+def print_help():
+    """ Prints out help """
+    msg = "add [project] [task] - adds task to project\n"
+    msg += "list - lists all projects and their items\n"
+    msg += "list [project] - lists items associated with that project\n"
+    msg += "projects - lists projects"
+    print(msg)
     exit(0)
 
+
 def get_proj_id(api, project):
+    """ Takes the api object and a project name and returns its id """
     resp = api.sync()
     # Get ID for project if it exists
     for resp_project in resp['projects']:
@@ -45,26 +48,38 @@ def get_proj_id(api, project):
     except NameError:
         return None
 
-def add_task(api, project_id, task):
-    item = api.items.add(task, project_id)
-    commit = api.commit()
+
+def add_task(api, project_name, task):
+    """ Takes a todoist api object, the project name, and the task and adds
+    the task to todoist """
+
+    project_id = get_proj_id(todoist, project_name)
+    if not project_id:
+        project_id = create_project(todoist, project_name)
+    api.items.add(task, project_id)
+    api.commit()
     print("Task added")
 
+
 def create_project(api, name):
-    resp = todoist.projects.add(name)
-    commit = api.commit()
+    """ Takes the api and the name of a project and creates it and returns
+    the new project's id """
+    api.projects.add(name)
+    api.commit()
     print("Created Project: {}".format(name))
 
     return get_proj_id(api, name)
 
+
 def list_all_tasks(api):
+    """ Takes the api and Returns a list of all tasks """
     resp = api.sync()
     projects = {}
     tasks = {}
     for project in resp['projects']:
         name = project['name']
-        id = project['id']
-        projects[id] = name
+        project_id = project['id']
+        projects[project_id] = name
 
     for item in resp['items']:
         if item['is_deleted']:
@@ -84,10 +99,13 @@ def list_all_tasks(api):
         for task in tasks:
             print("{} - {}".format(project, task))
 
+
 def list_tasks(api, project):
+    """ Takes the api and project name and returns the tasks associated with
+    that project """
     resp = api.sync()
     tasks = []
-    project_id  = ''
+    project_id = ''
     for resp_project in resp['projects']:
         if resp_project['name'].lower() == project.lower():
             project_id = resp_project['id']
@@ -109,6 +127,7 @@ def list_tasks(api, project):
 
 
 def list_projects(api):
+    """ Takes the API object and prints a list of projects """
     resp = api.sync()
     projects = []
     for project in resp['projects']:
@@ -117,29 +136,23 @@ def list_projects(api):
     projects.sort(key=lambda y: y.lower())
     print("\n".join(projects))
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        help()
-    todoist = connect()
+        print_help()
+    API = connect()
+    ACTION = sys.argv[1]
 
-    action = sys.argv[1]
-
-    if action == 'add':
+    if ACTION == 'add':
         if len(sys.argv) < 3:
-            help()
-        project_name = sys.argv[2]
-        task = ' '.join(sys.argv[3:])
-        project_id = get_proj_id(todoist, project_name)
-        if not project_id:
-            project_id = create_project(todoist, project_name)
-        add_task(todoist, project_id, task)
-    elif action == 'list':
+            print_help()
+        add_task(API, sys.argv[2], ' '.join(sys.argv[3:]))
+    elif ACTION == 'list':
         if len(sys.argv) == 2:
-            list_all_tasks(todoist)
+            list_all_tasks(API)
         else:
-            project = " ".join(sys.argv[2:])
-            list_tasks(todoist, project)
-    elif action == 'projects':
-        list_projects(todoist)
+            list_tasks(API, ' '.join(sys.argv[2:]))
+    elif ACTION == 'projects':
+        list_projects(API)
     else:
-      help()
+        print_help()
